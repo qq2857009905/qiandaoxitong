6d487346b33ea4b992ad88b823c8ae796b504d72
夜雪霜蓝 <285709905@qq.com>
2017-09-03 22:55:41 +0800

    再次更新

--- register/js/register.js
+++ register/js/register.js
@@ -2,18 +2,15 @@
  * Created by 夜雪霜蓝 on 2017/9/2.
  */
 $(document).ready(function () {
-
     $('#type').change(function () {
         //给用户类型禁用和启用注册信息
         var option=$('#type option:selected');
-        if(option.val()=='student'||option.val()=='teacher')
+        if(option.val()=='学生'||option.val()=='老师')
         {
             $('#adminNum').attr("disabled",'disabled');
         }
         else {
             $('#adminNum').attr("disabled",false);
         }
-    })
-
-    
-})
+    });
+});
